80425697da0c74169ab589393ac9d74f1daf2b26
pcysl5edgo <31692496+pCYSl5EDgo@users.noreply.github.com>
2025-12-09 20:48:03 +0900

    Add out blackslash overload

--- Pcysl5edgo.RedundantPath/BitSpan.cs
+++ Pcysl5edgo.RedundantPath/BitSpan.cs
@@ -96,6 +96,71 @@ public static class BitSpan
         }
     }
 
+    public static uint Get(ref ushort source, out uint dot, out uint backslash)
+    {
+        if (Vector512.IsHardwareAccelerated)
+        {
+            var v = Vector512.LoadUnsafe(ref source);
+            dot = (uint)Vector512.Equals(v, Vector512.Create((ushort)'.')).ExtractMostSignificantBits();
+            var compound = Vector512.Narrow(Vector512.Equals(v, Vector512.Create((ushort)'\\')), Vector512.Create((ushort)'/')).ExtractMostSignificantBits();
+            backslash = (uint)compound;
+            return (uint)(compound >>> 32);
+        }
+        else if (Vector256.IsHardwareAccelerated)
+        {
+            var v0 = Vector256.LoadUnsafe(ref source);
+            var v1 = Vector256.LoadUnsafe(ref source, 16);
+            var d = Vector256.Create((ushort)'.');
+            dot = Vector256.Narrow(Vector256.Equals(v0, d), Vector256.Equals(v1, d)).ExtractMostSignificantBits();
+            var b = Vector256.Create((ushort)'\\');
+            backslash = Vector256.Narrow(Vector256.Equals(v0, b), Vector256.Equals(v1, b)).ExtractMostSignificantBits();
+            var s = Vector256.Create((ushort)'/');
+            return Vector256.Narrow(Vector256.Equals(v0, s), Vector256.Equals(v1, s)).ExtractMostSignificantBits();
+        }
+        else if (Vector128.IsHardwareAccelerated)
+        {
+            var v0 = Vector128.LoadUnsafe(ref source);
+            var v1 = Vector128.LoadUnsafe(ref source, 8);
+            var v2 = Vector128.LoadUnsafe(ref source, 16);
+            var v3 = Vector128.LoadUnsafe(ref source, 24);
+            var d = Vector128.Create((ushort)'.');
+            var d0 = Vector128.Narrow(Vector128.Equals(v0, d), Vector128.Equals(v1, d)).ExtractMostSignificantBits();
+            var d1 = Vector128.Narrow(Vector128.Equals(v2, d), Vector128.Equals(v3, d)).ExtractMostSignificantBits();
+            dot = d0 | (d1 << 16);
+            var b = Vector128.Create((ushort)'\\');
+            var b0 = Vector128.Narrow(Vector128.Equals(v0, b), Vector128.Equals(v1, b)).ExtractMostSignificantBits();
+            var b1 = Vector128.Narrow(Vector128.Equals(v2, b), Vector128.Equals(v3, b)).ExtractMostSignificantBits();
+            backslash = b0 | (b1 << 16);
+            var s = Vector128.Create((ushort)'/');
+            var s0 = Vector128.Narrow(Vector128.Equals(v0, s), Vector128.Equals(v1, s)).ExtractMostSignificantBits();
+            var s1 = Vector128.Narrow(Vector128.Equals(v2, s), Vector128.Equals(v3, s)).ExtractMostSignificantBits();
+            return s0 | (s1 << 16);
+        }
+        else
+        {
+            uint _separator = default, _dot = default, _backslash = default;
+            for (int i = 0; i < 32; ++i)
+            {
+                switch (Unsafe.Add(ref source, i))
+                {
+                    case '.':
+                        _dot |= 1u << i;
+                        break;
+                    case '/':
+                        _separator |= 1u << i;
+                        break;
+                    case '\\':
+                        _backslash |= 1u << i;
+                        break;
+                }
+            }
+
+            dot = _dot;
+            backslash = _backslash;
+            return _separator;
+        }
+    }
+
     public static uint Get(ref ushort source, out uint dot, int length)
     {
         Debug.Assert((uint)length <= 32u);
@@ -126,6 +191,40 @@ public static class BitSpan
         }
 
         dot = _dot;
+        return separator;
+    }
+
+    public static uint Get(ref ushort source, out uint dot, out uint backslash, int length)
+    {
+        Debug.Assert((uint)length <= 32u);
+        uint separator = 0, _dot = 0, _backslash = 0;
+        int i = 0;
+        if (Vector128.IsHardwareAccelerated && length >= 16)
+        {
+            for (; i + Vector128<ushort>.Count <= length; i += Vector128<ushort>.Count)
+            {
+                var v = Vector128.LoadUnsafe(ref source, (nuint)i);
+                var compound = Vector128.Narrow(Vector128.Equals(v, Vector128.Create((ushort)'/')), Vector128.Equals(v, Vector128.Create((ushort)'.'))).ExtractMostSignificantBits();
+                separator |= ((uint)(byte)compound) << i;
+                _dot |= (compound >>> 8) << i;
+            }
+        }
+
+        for (; i < length; ++i)
+        {
+            switch (Unsafe.Add(ref source, i))
+            {
+                case '/':
+                    separator |= 1u << i;
+                    break;
+                case '.':
+                    _dot |= 1u << i;
+                    break;
+            }
+        }
+
+        dot = _dot;
+        backslash = _backslash;
         return separator;
     }
 
